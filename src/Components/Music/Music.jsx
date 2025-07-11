eb5846bb1e19c55bb7f821a19923354712eff7c0
Богдан Суров <bogdansurov@MacBook-Air-Bogdan.local>
2025-07-11 17:14:07 +0300

    Добавлена анимация, обозначающая играющий трек в плейлисте. Изменен эффект наведения на музыку;

--- src/Components/Music/Music.jsx
+++ src/Components/Music/Music.jsx
@@ -1,22 +1,23 @@
 import { useContext } from "react";
 import { CurrentMusicContext } from "@Providers/CurrentMusicContextProvider";
+import styles from "./Music.module.scss";
 
 export default function MusicComponent({ music }) {
   const { currentMusic, setCurrentMusic } = useContext(CurrentMusicContext);
 
   return (
     <div
-      className="font-semibold hover:bg-zinc-800 rounded-2xl p-2 mt-3 flex items-center cursor-pointer"
+      className={`${styles.music_card_wrapper} font-semibold hover:bg-zinc-800 rounded-2xl p-2 mt-3 flex items-center cursor-pointer`}
       onClick={() => {
         setCurrentMusic(music);
-        // audio.play();
       }}
     >
-      <div>
-        <img
-          src={music.getCover()}
-          className="h-[48px] w-[48px] rounded bg-stone-200 object-fill"
-        />
+      <div
+        className={`${music === currentMusic ? styles.play : ""} ${
+          styles.music_cover
+        }`}
+      >
+        <img src={music.getCover()} />
       </div>
       <div className="ms-3">
         <p>{music.getTitle()}</p>
